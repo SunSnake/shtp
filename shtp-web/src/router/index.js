8df5bb66aa8e48872ed9988501a301e7d6d68bab
Sunsnake <782749993@qq.com>
2019-04-07 11:56:40 +0800

    修改前端vue项目

--- shtp-web/src/router/index.js
+++ shtp-web/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Menubar from '@/components/Menubar'
+import Login from  '@/views/Login'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Menubar',
       component: Menubar
+    },
+    {
+      path: '/login ',
+      name: 'Login',
+      component: Login
     }
   ]
 })
