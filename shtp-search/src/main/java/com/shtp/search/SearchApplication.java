f6adcc427d00a4e631d0efe924478955e19bf466
Sunsnake <782749993@qq.com>
2019-05-05 22:26:35 +0800

    增加搜索引擎微服务框架，添加相应网关

---
+++ shtp-search/src/main/java/com/shtp/search/SearchApplication.java
@@ -0,0 +1,11 @@
+package com.shtp.search;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@MapperScan("com.shtp.search.mapper")
+public class SearchApplication {
+}
